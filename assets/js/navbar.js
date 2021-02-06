ab19fd99ba998f4dad8e4c14da44df4dac40c7d8
Marcos Oliveira <ziguifrido@gmail.com>
2021-02-05 22:34:56 -0400

    Dark Mode Toggle

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -1,3 +1,15 @@
+const theme = sessionStorage.getItem('theme') === 'dark' ? 'dark' : 'light'
+
+document.addEventListener("DOMContentLoaded", () => { 
+  ToggleDarkMode(theme)
+})
+
 const ToggleMenu = () => {
   document.getElementsByClassName("navbar-list")[0].classList.toggle('show')
+}
+
+const ToggleDarkMode = (theme) => {
+  document.body.className = ''
+  document.body.classList.add(theme)
+  sessionStorage.setItem('theme', theme)
 }
